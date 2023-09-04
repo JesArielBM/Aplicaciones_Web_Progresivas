5432fc9ad99853648a8174dac483d250945533a8
JsArielBM <20300139@uttt.edu.mx>
2023-09-04 17:44:36 -0600

    Factorial

---
+++ factorial.js
@@ -0,0 +1,17 @@
+const fact = (n) => {
+    let acum = 1;
+
+    for(let i=1; i<=n; i++){
+        acum*=i;
+    }
+
+    return acum;
+}
+
+const suma = (sumar) =>{
+    sumar(2,3);
+}
+
+function sumar(n1,n2){
+    console.log
+}
